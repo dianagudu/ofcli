cfb2225acbdf83b33d996a451ee0d4faac51cd94
Diana Gudu <diana.gudu@kit.edu>
2024-12-18 17:09:59 +0100

    fix failing tests

--- src/ofcli/utils.py
+++ src/ofcli/utils.py
@@ -77,7 +77,11 @@ class URL:
             return self._url == other.url()
         if isinstance(other, str):
             return self._url == HttpUrl(other)
-        if isinstance(other, pydantic_core._pydantic_core.Url):
+        if (
+            isinstance(other, pydantic_core._pydantic_core.Url)
+            or isinstance(other, pydantic_core.Url)
+            or isinstance(other, HttpUrl)
+        ):
             return self._url == other
         return False
 
